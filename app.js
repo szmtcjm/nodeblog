dc69b2630930e34f4acaa59e261b6497071093a1
szmtcjm <szmtcjm@126.com>
2014-06-05 17:48:19 +0800

    '20140605'

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var session = require('express-session');
 var path = require('path');
 var MongoStore = require('connect-mongo')(session);
 var settings = require('./settings');
+var flash = require('connect-flash');
 var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -16,6 +17,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(flash());
 
 app.use(favicon());
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,66 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+	router = express.Router(),
+	crypto = require('crypto'),
+	User = require('../models/user');
 
 // GET home page.
 router.get('/', function(req, res) {
-    res.render('index', { title: '主页' });
+    res.render('index', { 
+    	title: '主页',
+    	user: req.session.user,
+    	success: req.flash('success').toString(),
+    	error: req.flash('error').toString()
+    });
 });
 
 //注册页面
 router.get('/reg', function(req, res) {
-    res.render('reg', {title: '注册'});
+    res.render('reg', { 
+    	title: '注册',
+    	user: req.session.user,
+    	success: req.flash('success').toString(),
+    	error: req.flash('error').toString()
+    });
 });
 
 
 //注册请求
 router.post('/reg', function(req, res) {
+	var name = req.body.name,
+		password = req.body.password,
+		password_re = req.body['password-repeat'],
+		md5,
+		newUser;
+
+	if (password_re !== password) {
+		req.flash('error', '两次输入的密码不一致');
+		return res.redirect('/reg');
+	}
+
+	User.get(name, function(err, user) {
+		if (user) {
+			req.flash('error', '用户名已存在！');
+			return res.redirect('/reg');
+		}
+
+		md5 = crypto.createHash('md5');
+		password = md5.update(req.body.password).digest('hex');
+		newUser = new User({
+			name: req.body.name,
+			password: password,
+			email: req.body.email
+		});
+
+		newUser.save(function(err, user) {
+			if (err) {
+				req.flash('error', err);
+				return res.redirect('reg');
+			}
+			req.session.user = user;
+			req.flash('success', '注册成功');
+			res.redirect('/');
+		})
+	})
 });
 
 //登录页面
